12c5f8366bf58469920b4147c6a2ecc1652fc8c7
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-06-30 11:14:05 +1000

    feat: Make Dashboard redirect user without a token to Home page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import DashHeader from "../components/ui/DashHeader";
 import DashMain from "../components/ui/DashMain";
 import DashSideNav from "../components/ui/DashSideNav";
 import DashFooter from "../components/ui/DashFooter";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/");
+      return;
+    }
+  });
+
   return (
     <div className="relative w-full grow flex flex-col lg:grid lg:grid-cols-[200px_1fr] lg:grid-rows-[80px_1fr_30px] rounded-lg p-2 shadow-md">
       <DashHeader />
